d7e7b5fe85a98e68cada5621a27f13fe509fac4c
asqubit <49931291+asqubit@users.noreply.github.com>
2019-04-24 17:13:30 +0800

    list devices of DAQ

--- TEST/FACE/pyqum/instrument/serial/DC.py
+++ TEST/FACE/pyqum/instrument/serial/DC.py
@@ -1,4 +1,9 @@
 import nidaqmx
+from nidaqmx.system import System
+
+sys = System.local()
+for i,dev in enumerate(sys.devices):
+    print("%s. %s" %(i,dev))
 
 with nidaqmx.Task() as task:
     task.ai_channels.add_ai_voltage_chan("Dev1/ai0")
