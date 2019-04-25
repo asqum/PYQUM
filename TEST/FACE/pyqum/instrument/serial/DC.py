8b874e03b96a96366db33c8f5a3b4d2069fc0ca8
asqubit <49931291+asqubit@users.noreply.github.com>
2019-04-25 12:21:45 +0800

    added DC streaming

--- TEST/FACE/pyqum/instrument/serial/DC.py
+++ TEST/FACE/pyqum/instrument/serial/DC.py
@@ -1,9 +1,14 @@
-import nidaqmx
-from nidaqmx.system import System
-from nidaqmx.constants import TerminalConfiguration, AcquisitionType
-from nidaqmx.stream_readers import AnalogSingleChannelReader
-from numpy import ndarray
+import nidaqmx, random
 
+from nidaqmx.system import System
+from nidaqmx.constants import TerminalConfiguration, AcquisitionType, Edge
+
+from nidaqmx.stream_readers import (
+    AnalogSingleChannelReader, AnalogMultiChannelReader)
+from nidaqmx.stream_writers import (
+    AnalogSingleChannelWriter, AnalogMultiChannelWriter)
+
+from numpy import ndarray, array, zeros
 from pyqum.instrument.analyzer import curve
 from pyqum.instrument.toolbox import waveform
 
@@ -15,24 +20,68 @@ for i,dev in enumerate(sys.devices):
 device = sys.devices[(D[0])]
 print(device)
 
-X = waveform("0 to 10 *10 to 0 * 1500")
+X = waveform("0 to 10 *10 to 0 * 20")
 # print("X: %s" %X)
 V = []
 with nidaqmx.Task() as write_task, nidaqmx.Task() as read_task:
     write_task.ao_channels.add_ao_voltage_chan("Dev1/ao0")
-    # write_task.timing.cfg_samp_clk_timing(rate=1000, sample_mode=AcquisitionType.CONTINUOUS)
     read_task.ai_channels.add_ai_voltage_chan("Dev1/ai0", terminal_config=TerminalConfiguration.RSE, min_val=-10, max_val=10)
-    # read_task.timing.cfg_samp_clk_timing(rate=1000)
     for x in X:
         write_task.write(x, auto_start=True)
         V += read_task.read(1)
-        # stream
-        # reader = AnalogSingleChannelReader(read_task.in_stream)
-        # read_task.timing.cfg_implicit_timing(samps_per_chan=8)
-        # VS = reader.read_many_sample(ndarray(8))
 
 # print("V: %s" %V)
-# print("VS: %sV" %VS)
+curve(range(len(X)), X, "", "#", "Aout(V)")
+curve(range(len(V)), V, "", "#", "Ain(V)")
+
+# stream data
+number_of_samples = 3502
+print("number of samples: %s" %number_of_samples)
+sample_rate = 5000 #random.uniform(1000, 5000)
+with nidaqmx.Task() as write_task, nidaqmx.Task() as read_task, \
+        nidaqmx.Task() as sample_clk_task:
+
+        # Use a counter output pulse train task as the sample clock source
+        # for both the AI and AO tasks.
+        sample_clk_task.co_channels.add_co_pulse_chan_freq(
+            '{0}/ctr0'.format("Dev1"), freq=sample_rate)
+        sample_clk_task.timing.cfg_implicit_timing(
+            samps_per_chan=number_of_samples)
+
+        samp_clk_terminal = '/{0}/Ctr0InternalOutput'.format("Dev1")
+
+        write_task.ao_channels.add_ao_voltage_chan(
+            "Dev1/ao0", max_val=10, min_val=-10)
+        write_task.timing.cfg_samp_clk_timing(
+            sample_rate, source=samp_clk_terminal, active_edge=Edge.RISING,
+            samps_per_chan=number_of_samples)
+
+        read_task.ai_channels.add_ai_voltage_chan(
+            "Dev1/ai0", max_val=10, min_val=-10)
+        read_task.timing.cfg_samp_clk_timing(
+            sample_rate, source=samp_clk_terminal,
+            active_edge=Edge.FALLING, samps_per_chan=number_of_samples)
+
+        writer = AnalogSingleChannelWriter(write_task.out_stream)
+        reader = AnalogSingleChannelReader(read_task.in_stream)
+
+        # start writing waveform into AO
+        X = array(waveform("0 to 10 *%s to 0 * %s" %(round(number_of_samples/2), number_of_samples-round(number_of_samples/2))))
+        # X = array([random.uniform(-10, 10) for _ in range(number_of_samples)],dtype=float64)
+        writer.write_many_sample(X)
+
+        # Start the read and write tasks before starting the sample clock
+        # source task.
+        read_task.start()
+        write_task.start()
+        sample_clk_task.start()
+
+        V = zeros(number_of_samples)
+        reader.read_many_sample(
+            V, number_of_samples_per_channel=number_of_samples,
+            timeout=2)
+
+        print(V)
 
 curve(range(len(X)), X, "", "#", "Aout(V)")
 curve(range(len(V)), V, "", "#", "Ain(V)")
--- TEST/FACE/pyqum/instrument/toolbox.py
+++ TEST/FACE/pyqum/instrument/toolbox.py
@@ -1,7 +1,7 @@
 '''TOOLBOX for all other modules'''
 
 from time import sleep
-from numpy import array, append, zeros, prod, floor, inner, linspace
+from numpy import array, append, zeros, prod, floor, inner, linspace, float64
 
 def cdatasearch(Order, Structure):
     ''' Give the address of the data essentially!
@@ -25,32 +25,35 @@ def gotocdata(Address, Structure):
     return Order
 
 def waveform(command):
-    command = command.replace(" ","")
-    command = command.split("*")
-    C = []
-    for c in command:
-        C += c.split("to")
-
-    C = [float(x) for x in C] #float all the string elements!
-    start, wave = C[0], []
-    for target,num in zip(C[1::2],C[2::2]):
-        if num == C[-1]:
-            wave += list(linspace(start, target, num))
-        else:
-            wave += list(linspace(start, target, num, endpoint=False))
+    command = command.lower().replace(" ","").split("*")
+    C = [j for i in command for j in i.split('to')]
+    # rooting out wrong command:
+    try:
+        C = [float(x) for x in C] #float all the string elements!
+        start, wave = C[0], []
+        change = range(int(len(C[:-1])/2))
+        for i,target,num in zip(change,C[1::2],C[2::2]):
+            wave += list(linspace(start, target, int(num), endpoint=bool(i==change[-1]), dtype=float64))
             start = target
+    except: 
+        raise
+        print("Invalid command")
+        pass
         
     return wave
 
 
 def test():
-    for i in range(150):
-        print("decoding data-%s into c-%s and back into %s" 
-        %(i, cdatasearch(i, [8,10,10,2]), gotocdata(cdatasearch(i, [8,10,10,2]), [8,10,10,2])))
+    # for i in range(150):
+    #     print("decoding data-%s into c-%s and back into %s" 
+    #     %(i, cdatasearch(i, [8,10,10,2]), gotocdata(cdatasearch(i, [8,10,10,2]), [8,10,10,2])))
         # sleep(0.3)
-    command = "0 to  10  * 7 to  20 *15 to35*  13"
+    command = "0 to  10  * 7 TO  20 *15 to35*  13"
     print("Waveform of length %s is:\n %s" %(len(waveform(command)), waveform(command)))
+    number_of_samples = 101
+    print(array(waveform("0 to 10 *%s to 0 * %s" %(round(number_of_samples/2), number_of_samples-round(number_of_samples/2)))))
+    print(array(waveform("0 to 10 *51 to 0 * 51")))
     return
 
 
-# test()
+test()
