b36778650403aeae78d98538e40b3e6243c43294
asqubit <49931291+asqubit@users.noreply.github.com>
2019-05-28 17:46:26 +0800

    update

--- TEST/FACE/pyqum/instrument/serial/DC.py
+++ TEST/FACE/pyqum/instrument/serial/DC.py
@@ -31,20 +31,41 @@ __status__ = "development"
 pyfilename = inspect.getfile(inspect.currentframe()) # current pyscript filename (usually with path)
 
 # INITIALIZATION
-def openall(aoi=0, aof=3, aii=0, aif=31, read=True, write=False):
+def openall(aoi=0, aof=3, aii=0, aif=31, read=True, write=False, clock=False, sample_rate=500000, initial_delay=0, duty_cycle=0.5, samps_per_chan=1):
     '''
     4 output channels, 32 input channels
+    duty cycle = pulse width / pulse period
     '''
     ad = address()
     rs = ad.lookup(mdlname) # Instrument's Address
-    write_task, read_task = None, None
-    if write:
-        write_task = nidaqmx.Task()
-        write_task.ao_channels.add_ao_voltage_chan("%s/ao%s:%s" %(rs,aoi,aof))
+    read_task, write_task, clock_task, max_samp_rate, writer, reader = None, None, None, None, None, None
     if read:
         read_task = nidaqmx.Task()
         read_task.ai_channels.add_ai_voltage_chan("%s/ai%s:%s" %(rs,aii,aif), terminal_config=TerminalConfiguration.RSE, min_val=-10, max_val=10)
-    return write_task, read_task
+    if write:
+        write_task = nidaqmx.Task()
+        write_task.ao_channels.add_ao_voltage_chan("%s/ao%s:%s" %(rs,aoi,aof))
+        # write_task.ao_channels.add_ao_func_gen_chan
+    if clock:
+        # one-shot data streaming
+        # Use a counter output pulse train task as the sample clock source for both the AI and AO tasks.
+        clock_task = nidaqmx.Task()
+        clock_task.co_channels.add_co_pulse_chan_freq('{0}/ctr0'.format(rs), freq=sample_rate, initial_delay=initial_delay, duty_cycle=duty_cycle)
+        clock_task.timing.cfg_implicit_timing(sample_mode=AcquisitionType.FINITE, samps_per_chan=samps_per_chan)
+        samp_clk_terminal = '/{0}/Ctr0InternalOutput'.format(rs)
+        write_task.timing.cfg_samp_clk_timing(sample_rate, source=samp_clk_terminal, active_edge=Edge.RISING, samps_per_chan=samps_per_chan)
+        read_task.timing.cfg_samp_clk_timing(sample_rate, source=samp_clk_terminal, active_edge=Edge.FALLING, samps_per_chan=samps_per_chan)
+        max_samp_rate = read_task.timing.samp_clk_max_rate
+        print("Max reading rate: %s" %max_samp_rate)
+        # Single Channel:
+        writer = AnalogSingleChannelWriter(write_task.out_stream)
+        # reader = AnalogSingleChannelReader(read_task.in_stream)
+        # Multi Channel:
+        # writer = AnalogMultiChannelWriter(write_task.out_stream)
+        reader = AnalogMultiChannelReader(read_task.in_stream)
+
+    pack = dict(write_task=write_task, read_task=read_task, clock_task=clock_task, max_samp_rate=max_samp_rate, writer=writer, reader=reader)
+    return pack
 
 def closeall(w, r):
     w.close()
@@ -53,7 +74,7 @@ def closeall(w, r):
 
 class amplifier:
     def __init__(self):
-        self.sense = openall(aof=1, aii=6, aif=15)[1]
+        self.sense = openall(aof=0, aii=6, aif=15)['read_task']
         with open(Path(pyfilename).parent / 'calibration.json') as ad:
             self.book = json.load(ad)
 
@@ -76,10 +97,41 @@ class amplifier:
     def close(self):
         self.sense.close()
 
+class measure:
+    '''perform all sort of dc measurements
+    sample_rate: max 500K spread over all channels
+    '''
+    def __init__(self, sample_rate=500000, initial_delay=0, duty_cycle=0.5, samps_per_chan=1):
+        self.sample_rate = sample_rate
+        self.initial_delay = initial_delay
+        self.duty_cycle = duty_cycle
+        self.samps_per_chan = samps_per_chan
+        self.electrode = openall(aof=0, aii=0, aif=5, write=True, clock=True, sample_rate=sample_rate, initial_delay=initial_delay, duty_cycle=duty_cycle, samps_per_chan=samps_per_chan)
+
+    def IVb(self, active_waveform):
+        write_task = self.electrode['write_task']
+        read_task = self.electrode['read_task']
+        clock_task = self.electrode['clock_task']
+        writer = self.electrode['writer']
+        reader = self.electrode['reader']
+        # start writing waveform into AO
+        writer.write_many_sample(active_waveform)
+
+        # Start the read and write tasks before starting the sample-clock-source task.
+        read_task.start()
+        write_task.start()
+        clock_task.start()
+
+        read_values = zeros((6, self.samps_per_chan))
+        reader.read_many_sample(read_values, number_of_samples_per_channel=self.samps_per_chan, timeout=88)
+        print(range(read_values.size))
+
+        return read_values
+
 
 # w.write([0, 0, 0, 0], auto_start=True)
-
 def test():
+    # Test Amplifier settings
     A = amplifier()
     for i in range(3):
         print("\nSensing Hard Panel #%s:" %(i+1))
@@ -96,70 +148,23 @@ def test():
         print("Vg Mode 2: %s" %A.VgMode2)
     A.close()
 
+    # Test Streaming IV-curve
+    X0, X1, X3 = waveform("0 to 10 *1000 to 0 * 2000"), waveform("0 to 5 *700 to 10*1300 to 0 * 1000"), waveform("0 to 3 *700  to 1*1500 to 7*500 to 0 * 300")
+    # number_of_samples = X0.count #should be the same for both channels
+    # Waveform_bundles = array([X0.data, X1.data, X3.data])
+    # print("Waveform_bundles:\n%s" %Waveform_bundles)
+    # M = measure(samps_per_chan=X0.count)
+    # M.IVb(X0)
+    # curve([range(X0.count),range(read_values[3].size)], [X0.data,list(read_values[3])], "Channel #0", "arb time", "V(V)", ["-k","or"])
+    
+    
+    
+    return
+
 test()
 
-A = input("pause")
+# A = input("pause")
+# with nidaqmx.Task() as write_task, nidaqmx.Task() as read_task, \
+#     nidaqmx.Task() as sample_clk_task:
 
-# stream data
-X0, X1, X3 = waveform("0 to 10 *1000 to 0 * 2000"), waveform("0 to 5 *700 to 10*1300 to 0 * 1000"), waveform("0 to 3 *700  to 1*1500 to 7*500 to 0 * 300")
-X = array([X0.data, X1.data, X3.data])
-number_of_samples = X0.count #should be the same for both channels
-print("X:\n%s" %X)
-sample_rate = 10000 #max 500K over all channels
-with nidaqmx.Task() as write_task, nidaqmx.Task() as read_task, \
-        nidaqmx.Task() as sample_clk_task:
-        # Use a counter output pulse train task as the sample clock source
-        # for both the AI and AO tasks.
-        # duty cycle = pulse width / pulse period
-        sample_clk_task.co_channels.add_co_pulse_chan_freq(
-            '{0}/ctr0'.format("Dev1"), freq=sample_rate, initial_delay=0, duty_cycle=0.5)
-        sample_clk_task.timing.cfg_implicit_timing(
-            sample_mode=AcquisitionType.FINITE,
-            samps_per_chan=number_of_samples)
-
-        samp_clk_terminal = '/{0}/Ctr0InternalOutput'.format("Dev1")
-
-        write_task.ao_channels.add_ao_voltage_chan(
-            "Dev1/ao0:1, Dev1/ao3", max_val=10, min_val=-10)
-        write_task.timing.cfg_samp_clk_timing(
-            sample_rate, source=samp_clk_terminal, active_edge=Edge.RISING,
-            samps_per_chan=number_of_samples)
-        # write_task.ao_channels.add_ao_func_gen_chan
-
-        read_task.ai_channels.add_ai_voltage_chan(
-            "Dev1/ai0:1, Dev1/ai17", terminal_config=TerminalConfiguration.RSE, max_val=10, min_val=-10)
-        read_task.timing.cfg_samp_clk_timing(
-            sample_rate, source=samp_clk_terminal,
-            active_edge=Edge.FALLING, samps_per_chan=number_of_samples)
-        # read_task.channels.ai_averaging_win_size = 3 #FPGA
-        print("Max reading rate: %s" %read_task.timing.samp_clk_max_rate)
-
-        # Single Channel:
-        # writer = AnalogSingleChannelWriter(write_task.out_stream)
-        # reader = AnalogSingleChannelReader(read_task.in_stream)
-
-        # Multi Channel:
-        writer = AnalogMultiChannelWriter(write_task.out_stream)
-        reader = AnalogMultiChannelReader(read_task.in_stream)
-
-        # start writing waveform into AO
-        writer.write_many_sample(X)
-
-        # Start the read and write tasks before starting the sample clock
-        # source task.
-        read_task.start()
-        write_task.start()
-        sample_clk_task.start()
-
-        V = zeros((3,number_of_samples))
-        reader.read_many_sample(
-            V, number_of_samples_per_channel=number_of_samples,
-            timeout=88)
-
-        # print(V)
-
-print(range(V.size))
-curve([range(X0.count),range(V[0].size)], [X0.data,list(V[0])], "Channel #0", "arb time", "V(V)", ["-k","or"])
-curve([range(X1.count),range(V[1].size)], [X1.data,list(V[1])], "Channel #1", "arb time", "V(V)", ["-k","or"])
-curve([range(X3.count),range(V[2].size)], [X3.data,list(V[2])], "Channel #3->17", "arb time", "V(V)", ["-k","or"])
 
