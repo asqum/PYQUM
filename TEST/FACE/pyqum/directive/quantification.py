940cb13230577a81249315f1aa5d28a9e3a83bdd
TsungYenChu <tsungyenchu@gmail.com>
2022-01-17 15:20:45 +0800

    fixed base

--- TEST/FACE/pyqum/directive/quantification.py
+++ TEST/FACE/pyqum/directive/quantification.py
@@ -25,7 +25,7 @@ from scipy.optimize import curve_fit
 from scipy.stats import linregress
 
 #from si_prefix import si_format, si_parse
-from numpy import cos, sin, pi, polyfit, poly1d, array, roots, isreal, sqrt, mean, std, histogram
+from numpy import cos, sin, pi, polyfit, poly1d, polyval, array, roots, isreal, sqrt, mean, std, histogram, average, newaxis, float64, any, var
 
 # Load instruments
 # Please Delete this line in another branch (to: @Jackie)
@@ -33,9 +33,13 @@ from pyqum.directive import calibrate
 from pyqum.mission import get_measurementObject
 
 # Fitting
-from resonator_tools.circuit import notch_port
 from collections import defaultdict
-
+from resonator_tools.circuit import notch_port
+from resonator_tools.utilities import plotting, save_load, Watt2dBm, dBm2Watt
+from resonator_tools.circlefit import circlefit
+from resonator_tools.calibration import calibration
+from sklearn.metrics import r2_score
+import pandas as pd
 # Save file
 from scipy.io import savemat
 
@@ -793,6 +797,25 @@ class Common_fitting():
 						self.fitResult[k]["value"][i] = popt[ki]
 						self.fitResult[k]["error"][i] = perr[ki]
 
+def fit_plot(i,ax,coef):return coef[0]*ax*ax+coef[1]*ax+coef[2]
+
+def fit_sin(tt, yy):
+	'''Fit sin to the input time sequence, and return fitting parameters "amp", "omega", "phase", "offset", "freq", "period" and "fitfunc"'''
+	tt = array(tt)
+	yy = array(yy)
+	ff = fft.fftfreq(len(tt), (tt[1]-tt[0]))   # assume uniform spacing
+	Fyy = abs(fft.fft(yy))
+	guess_freq = abs(ff[argmax(Fyy[1:])+1])   # excluding the zero frequency "peak", which is related to offset
+	guess_amp = std(yy) * 2.**0.5
+	guess_offset = mean(yy)
+	guess = array([guess_amp, 2.*pi*guess_freq, 0., guess_offset])
+
+	def sinfunc(t, A, w, p, c):  return A * sin(w*t + p) + c
+	popt, pcov = curve_fit(sinfunc, tt, yy, p0=guess)
+	A, w, p, c = popt
+	f = w/(2.*pi)
+	fitfunc = lambda t: A * sin(w*t + p) + c
+	return {"amp": A, "omega": w, "phase": p, "offset": c, "freq": f, "period": 1./f, "fitfunc": fitfunc, "maxcov": max(pcov), "rawres": (guess,popt,pcov)}
 
 class Autoflux():
 
@@ -801,10 +824,140 @@ class Autoflux():
 		self.quantificationObj = quantificationObj
 
 		# Fit
-		self.fitCurve = {}
-		self.fitResult = {}
+		self.real = []
+		self.imag = []
 
-		self._fitParameters = None
-		#self._init_fitResult()
+
+	def do_analysis( self ):
+		self.x = self.quantificationObj.rawdata["x"]
+		self.y =self.quantificationObj.independentVars["yAxisKey"]
+		self.real = self.quantificationObj.rawdata["iqSignal"].real
+		self.imag = self.quantificationObj.rawdata["iqSignal"].imag
 		
-		# self._init_fitCurve()
+		#---------------changeable variable---------------
+		# x(ki) = g*g/delta
+		ki = 0.003
+		fdress = 8.125
+		
+
+		#---------------prepare data ---------------
+		df1=pd.DataFrame()
+		for j in range(len(self.x)):
+			df =pd.DataFrame({"Frequency":self.y,"Flux-Bias":self.x,"i":self.real,"q":self.imag}).sort_values(["Frequency","Flux-Bias"],ascending=True)
+			port1 = notch_port(f_data=df["Frequency"].values,z_data_raw=df["i"]+1j*df["q"])
+			# port1.plotrawdata()
+			port1.autofit()
+			#     port1.plotall()
+			#     display(pd.DataFrame([port1.fitresults]).applymap(lambda x: "{0:.2e}".format(x)))
+			df1 = df1.append(pd.DataFrame([port1.fitresults]), ignore_index = True)
+		df1.insert(loc=0, column='flux', value=self.x*10**6)
+
+		#---------------drop the outward data---------------
+		f_min,f_max = min(self.y),max(self.y)
+		valid = df1[(df1['fr']>= f_min)&(df1['fr']<= f_max)]
+		valid.reset_index(inplace=True)
+
+		#---------------determine the sin_wave or arcsin_wave
+		if valid.diff(periods=1, axis=0)['fr'].var() >2.5*10**-5 and max(valid['fr'])-min(valid['fr'])>0.002 :twokind=1
+		elif valid.diff(periods=1, axis=0)['fr'].var() <2.5*10**-5 and max(valid['fr'])-min(valid['fr'])<0.002:twokind=0
+		else:raise ValueError('I do not know how')
+		if twokind:
+		#     print('fr>fc and fr<fc')
+			#---------------using difference to find upward points---------------
+			dif=valid.diff(periods=1, axis=0)['fr']; dif[0] = 0
+			last,count = -1,-1
+			valid_u, coef_u, poly_u, fit_u, x0_u, r2_u,ax_u, wave,fd = [],[],[],[],[],[],[],[],[]
+			for i in valid[(dif>ki)]['index'].keys():
+				if last == -1:last = i
+				else:
+					if len(valid[(valid['fr']>fdress)&(valid['index']>last)&(valid['index']<i)])<3:
+						last = i
+						pass
+					else:
+						wave.append(count)
+						valid_u.append(valid[(valid['fr']>fdress)&(valid['index']>last-1)&(valid['index']<i)])
+						last = i
+				count+=1   
+			valid_u.append(valid[(valid['fr']>fdress)&(valid['index']>last)&(valid['index']<max(self.x)*10**6)])
+			wave.append(count)
+			for i in range(len(valid_u)):
+				coef_u.append(polyfit(valid_u[i]['flux'],valid_u[i]['fr'],2))
+				poly_u.append(poly1d(coef_u[i]))
+				fit_u.append(polyval(coef_u[i],valid_u[i]['flux']))
+				ax_u.append(range(int(valid_u[i]['flux'][valid_u[i]['flux'].keys()[0]])-5,int(valid_u[i]['flux'][valid_u[i]['flux'].keys()[-1]])+5))
+				fd.append(min(valid_u[i]['fr']))
+				r2_u.append(r2_score(valid_u[i]['fr'], fit_u[i]))
+				if coef_u[i][0]!=0:
+					x0_u.append(round(-0.5*coef_u[i][1]/coef_u[i][0],2))
+				else:
+					raise ValueError('Fail to fit.')
+
+			#---------------using Squeeze Theorem find the only one downward function between the minimum points of two upward function---------------
+			if len(x0_u)<2 and len(wave)<2:
+				raise ValueError('The data does not have enough points to find wavelength. Please add more point')
+
+			wavelength,cavity_range,valid_ca,coef_ca, poly_ca, fit_ca, x0_ca, r2_ca ,fc,ax_ca = [],[],[],[],[],[],[],[],[],[]
+			success =-1
+			for i in range(len(x0_u)-1):wavelength.append((x0_u[i+1]-x0_u[i])/(wave[i+1]-wave[i]))
+			avg_wavelength = average(wavelength)
+			for i in range(wave[-1]):
+				if wave[i] == i:
+					cavity_range.append([x0_u[i],x0_u[i]+avg_wavelength])
+					success = i
+				elif wave[i]!= i:
+					if wave[i]== i+1:
+						cavity_range.append([x0_u[i]-avg_wavelength,x0_u[i]])
+					elif success != -1:
+						cavity_range.append([x0_u[success]+(i-success)*avg_wavelength,x0_u[success]+(i-success+1)*avg_wavelength])
+					else:
+						pass
+			# print("cavity_range = ",cavity_range)
+			for i in range(len(cavity_range)):
+				valid_ca.append(valid[(valid['fr']<average(fd)-ki)&(valid['flux']>cavity_range[i][0])&(valid['flux']<cavity_range[i][1])])
+				coef_ca.append(polyfit(valid_ca[i]['flux'],valid_ca[i]['fr'],2))
+				poly_ca.append(poly1d(coef_ca[i]))
+				fit_ca.append(polyval(coef_ca[i],valid_ca[i]['flux']))
+				ax_ca.append(range(int(valid_ca[i]['flux'][valid_ca[i]['flux'].keys()[0]])-5,int(valid_ca[i]['flux'][valid_ca[i]['flux'].keys()[-1]])+5))
+				fc.append(max(valid_ca[i]['fr']))
+				r2_ca.append(r2_score(valid_ca[i]['fr'], fit_ca[i]))
+				if coef_ca[i][0]!=0:
+					x0_ca.append(round(-0.5*coef_ca[i][1]/coef_ca[i][0],2))
+				else:
+					raise ValueError('Fail to fit.')
+			#---------------print the conclusion---------------
+			print("R2_ERROR :\n\t","{:<18}".format("Avg_Dressed Cavity")," : ","{:.4f}".format(average(r2_u)*100),"%\n\t","{:<18}".format("Avg_Cavity")," : ","{:.4f}".format(average(r2_ca)*100),"%\n")
+			print("{:<11}".format("Avg_fdress")+" : "+ "{:.4f}".format(average(fd))+" GHz "+" ; "+"{:<11}".format("Var_fdress")+" : "+ "{:.4f}".format(var(fd)*10**6)+" kHz")
+			print("{:<11}".format("Avg_fcavity")+" : "+ "{:.4f}".format(average(fc))+" GHz "+" ; "+"{:<11}".format("Var_fcavity")+" : "+ "{:.4f}".format(var(fc)*10**6)+" kHz\n")
+			print("{:^29}".format("Expected")+"|"+"{:^14}".format("Actual")+"\n"+"-"*44)
+			print("{:^14}".format("flux(uV/A)")+"|"+"{:^14}".format("freq(GHz)")+"|"+"{:^14}".format("fc(GHz)")+"\n"+"-"*44)
+			for i in range(len(valid_u)):
+				print("{:^14.2f}".format(x0_u[i])+"|"+"{:^14.4f}".format(poly_u[i](x0_u[i]))+"|"+"{:^14.4f}".format(min(valid_u[i]['fr'])))
+			print("-"*44)
+			for i in range(len(cavity_range)):
+				print("{:^14.2f}".format(x0_ca[i])+"|"+"{:^14.4f}".format(poly_ca[i](x0_ca[i]))+"|"+"{:^14.4f}".format(min(valid_ca[i]['fr'])))
+			fc = float("{:.6f}".format(average(fc)))
+			fd = float("{:.6f}".format(average(fd)))
+			offset = x0_ca[0]
+
+		else:
+		#     print('sin')
+			fc = float("{:.6f}".format(min(valid['fr'])))
+			fd = float("{:.6f}".format(max(valid['fr'])))
+			offset = valid[(valid['fr']==min(valid['fr']))]['flux'].values[0]
+			#https://stackoverflow.com/questions/16716302/how-do-i-fit-a-sine-curve-to-my-data-with-pylab-and-numpy
+			res = fit_sin(valid['flux'],valid['fr'])
+			print("{:^16}".format("Amplitude")+" = "+ "{:>8.4f}".format(float(res['amp']))+" GHz")
+			print("{:^16}".format("Angular freq.")+" = "+ "{:>8.4f}".format(float(res['omega']))+" uV/A")
+			print("{:^16}".format("phase")+" = "+ "{:>8.4f}".format(float(res['phase']))+" uV/A")
+			print("{:^16}".format("offset")+" = "+ "{:>8.4f}".format(float(res['offset']))+" GHz")
+			print("{:^16}".format("Max. Covariance")+" = "+ "{:>8.4f}".format(float(res['maxcov'])))
+			x = linspace(0,200,200)
+			fc = float("{:.6f}".format(fc))
+			fd = float("{:.6f}".format(fd))
+			# print(type(offset))
+
+		print("")
+		print("{:<23}".format("Final_dressed frquency"), " : " , "{:.4f}".format(fd) ,"GHz")
+		print("{:<23}".format("Final_cavity frquency"), " : " , "{:.4f}".format(fc) ,"GHz")
+		print("{:<23}".format("Final_x(ki)"), " : " , "{:.4f}".format((fd-fc)*1000) ,"MHz")
+		print("{:<23}".format("Final_offset flux")," : ",offset,"uV/A")
