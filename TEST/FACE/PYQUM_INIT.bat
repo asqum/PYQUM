df27e41a485e32ceada8ed7d0e8a522ac0cbed6d
asqubit <49931291+asqubit@users.noreply.github.com>
2022-03-03 21:36:11 +0800

    Update PYQUM_INIT.bat

--- TEST/FACE/PYQUM_INIT.bat
+++ TEST/FACE/PYQUM_INIT.bat
@@ -123,6 +123,21 @@ REM PAUSE
 
 :production_2
     ECHO STARTING APP as Production Web #2
+
+    rem Define here the path to your conda installation
+    set CONDAPATH=C:\Users\ASQUM_2\anaconda3
+    rem Define here the name of the environment
+    ::set ENVNAME=base
+    set ENVNAME=PYQUM-server-offline
+
+    rem Activate the conda environment
+    rem Using call is required here, see: https://stackoverflow.com/questions/24678144/conda-environments-and-bat-files
+    ECHO Conda path: %CONDAPATH%\Scripts\activate.bat 
+    ECHO Environment name: %ENVNAME%
+    call %CONDAPATH%\Scripts\activate.bat %ENVNAME%
+
+
+    rem Run a python script in that environment
     python pqrun.py production 5302
     goto tq
 
