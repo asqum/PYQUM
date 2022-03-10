3656931e73c21bf7f05ec89e9d12bd15921d72f6
shiau109 <shiau109@gmail.com>
2022-03-10 15:56:35 +0800

    Finish QPU translate to QPC

--- TEST/BETAsite/ASQPU/ASQPU/hardware_information.py
+++ TEST/BETAsite/ASQPU/ASQPU/hardware_information.py
@@ -1,11 +1,42 @@
 # Numpy
 # 
 from calendar import c
-from numpy import linspace, arange
+from sys import modules
+from numpy import linspace, arange, shape
 # Numpy common math function
 from numpy import exp
 # Numpy constant
 from numpy import pi
+
+
+
+class PhysicalChannel():
+    
+    #channelTypes = ["PulseRO","PulseCtrl","CWRO","CWCtrl"]
+    deviceTypes = ["DAC","ADC","SG","DC","VNA","SA","IQMixer","DRWiring"]
+    roles = ["Control","Readout"]
+
+    def __init__( self, id ):
+        self.id = id
+        #self.role = role
+        self.coupled = []
+        self.device = {}
+
+    def add_device( self, deviceIDs, deviceType ):
+        print(f"Add devices {deviceIDs} with type {deviceType} to physical channel {self.id}.")
+        if deviceType in PhysicalChannel.deviceTypes:
+            if deviceType not in self.device.keys():
+                self.device[deviceType]=[]
+                print(f"Device type {deviceType} didn't exist, creating now.")
+
+            for dID in deviceIDs:
+                if dID not in self.device[deviceType]:
+                    self.device[deviceType].append(dID)
+                    print(f"Device {dID} is added successfully.")
+                else:
+                    print(f"Device {dID} is already registered.")
+        else: 
+            print(f"Can't recognize device type {deviceType}.")
 
 class QuantumProcessUnit:
     def __init__ ( self, id ):
@@ -16,6 +47,12 @@ class QuantumProcessUnit:
 
     def get_IDList_PhysicalChannel( self ):
         return list(self.ChannelSet.keys())
+
+    def get_PhysicalChannel_byID( self, channelID ) -> PhysicalChannel:
+        if self.isExist_PhysicalChannel( channelID ):
+            return self.ChannelSet[channelID]
+        else:
+            return None
 
     def isExist_PhysicalChannel( self, channelID ):
         if channelID in self.get_IDList_PhysicalChannel():
@@ -46,14 +83,14 @@ class QuantumProcessUnit:
         if not self.isExist_PhysicalQubit(qubitID):
             #print(f"Create Qubit {qubitID}.")
             newQubit = PhyQubit(qubitID)
-            self.QubitSet[qubitID]=newQubit
+            self.QubitSet[qubitID] = newQubit
+
             print(f"Register Qubit {qubitID} in QPU {self.id}.")
             if channelIDList != None:
                 self.assign_channelToQubit(qubitID,channelIDList)
 
         else:
             print(f"Qubit {qubitID} is registered.")
-        return self.QubitSet[qubitID]
 
     def assign_channelToQubit( self, qubitID, channelIDList ):
         qubitChannel = {}
@@ -62,7 +99,6 @@ class QuantumProcessUnit:
                 #print(f"Assigning {channelID}...")
                 if self.isExist_PhysicalChannel(channelID):
                     qubitChannel[channelID]=self.ChannelSet[channelID]
-                    print(f"Assign {channelID} to {qubitID}.")
                 else:
                     print(f"Warning: The channel {channelID} didn't register in QPU {self.id}.")
 
@@ -73,17 +109,16 @@ class QuantumProcessUnit:
 
 
 
-
 class PhyQubit():
-    def __init__ ( self, qid, PhysicalChannel={} ):
+    def __init__ ( self, qid ):
 
         self.id = qid
-        self.PhysicalChannel = PhysicalChannel
+        self.phyCh = {}
         self.init_intrinsicProperties()
         self.operationCondition = []
 
     def get_IDList_PhysicalChannel( self ):
-        return list(self.PhysicalChannel.keys())
+        return list(self.phyCh.keys())
 
     def isExist_PhysicalChannel( self, channelID ):
         if channelID in self.get_IDList_PhysicalChannel():
@@ -96,9 +131,9 @@ class PhyQubit():
         for phyCh in phyChDict.keys():
             if not self.isExist_PhysicalChannel(phyCh):
                 print(f"Assign physical channel {phyCh} to Qubit {self.id} successfully.")
-                self.PhysicalChannel.update({phyCh:phyChDict[phyCh]})
+                self.phyCh.update({phyCh:phyChDict[phyCh]})
             else:
-                print(f"Physical channel {phyCh} is already in QPU {self.id}.")
+                print(f"Physical channel {phyCh} is already in Qubit {self.id}.")
 
     def set_intrinsicProperties( self, properties ):
         self.intrinsicProperties.update(properties)
@@ -145,33 +180,6 @@ class PhyQubit():
             self.operationCondition.append(opc)
 
 
-class PhysicalChannel():
-    
-    #channelTypes = ["PulseRO","PulseCtrl","CWRO","CWCtrl"]
-    deviceTypes = ["DAC","ADC","SG","VNA","SA","IQMixer","DRWiring"]
-    roles = ["Control","Readout"]
-
-    def __init__( self, id, role="Readout" ):
-        self.id = id
-        self.role = role
-        self.coupled = []
-        self.device = {}
-
-    def add_device( self, deviceIDs, deviceType ):
-        print(f"Add devices {deviceIDs} with type {deviceType} to physical channel {self.id}.")
-        if deviceType in PhysicalChannel.deviceTypes:
-            if deviceType not in self.device.keys():
-                self.device[deviceType]=[]
-                print(f"Device type {deviceType} didn't exist, creating now.")
-
-            for dID in deviceIDs:
-                if dID not in self.device[deviceType]:
-                    self.device[deviceType].append(dID)
-                    print(f"Device {dID} is added successfully.")
-                else:
-                    print(f"Device {dID} is already registered.")
-        else: 
-            print(f"Can't recognize device type {deviceType}.")
 
 
 # API
@@ -191,7 +199,7 @@ def create_QPU_by_route( qpuid, routeString ):
         phyChID = qCh[1]
         phyCh = PhysicalChannel(phyChID)
 
-        if len(qCh) == 2: phyCh.coupled = ["None"]
+        if len(qCh) == 2: phyCh.coupled = [phyChID]
         else: phyCh.coupled = qCh[2].split("+")
  
         devices = singleString.split(":")[1].split(",")
@@ -212,12 +220,75 @@ def create_QPU_by_route( qpuid, routeString ):
 
     return newQPU
 
+def get_QPUinstrument( qpu:QuantumProcessUnit ):
+    QPC_list = ["DAC","ADC","SG","DC"]
+
+    instrumentDict = dict.fromkeys(QPC_list,"")
+
+    for devicesType in QPC_list:
+        devicesList = []
+        for chid in qpu.get_IDList_PhysicalChannel():
+            pch = qpu.get_PhysicalChannel_byID(chid)
+            if devicesType in pch.device.keys():
+                deviceIDs = pch.device[devicesType] 
+
+                for did in deviceIDs:
+                    moduleID=did.split("-")[0]
+                    if moduleID not in devicesList: 
+                        devicesList.append(moduleID)
+
+
+        instrumentDict[devicesType] = devicesList
+
+    return instrumentDict
+
+def get_QPUwiring( qpu:QuantumProcessUnit ):
+    """
+    Translate the information in QPU to the format of the QPC
+    """
+    QPC_list = ["DAC","ADC","SG","DC"]
+    instr_organized = dict.fromkeys(QPC_list,"")
+
+    instrumentDict = get_QPUinstrument(qpu)
+    instr_organized["CH"] = ""
+    instr_organized["ROLE"] = ""
+
+    for devicesType in instrumentDict.keys():
+        instrumentList = instrumentDict[devicesType]
+        CHstringList = []
+        ROLEstringList = []
+        for instrument in instrumentList:
+            channelList = []
+            ROLElist = []
+            for chid in qpu.get_IDList_PhysicalChannel():
+                pch = qpu.get_PhysicalChannel_byID(chid)
+
+                if devicesType in pch.device.keys():
+                    deviceIDs = pch.device[devicesType] 
+                    for i, did in enumerate(deviceIDs):
+                        deviceInfo = did.split("-")
+                        if deviceInfo[0] == instrument and shape(deviceInfo)[-1]>1:
+                            channelID = did.split("-")[1]
+                            channelList.append(channelID)
+                            ROLElist.append(pch.coupled[i])
+            if shape(channelList)[-1]>0:
+                CHstringList.append("/".join(channelList))
+                ROLEstringList.append("/".join(ROLElist))
+            
+        if shape(CHstringList)[-1]>0:
+            CHstring = f"{devicesType}:{','.join(CHstringList)}"
+            ROLEstring = f"{devicesType}:{','.join(ROLEstringList)}"
+        instr_organized[devicesType] = ",".join(instrumentList)
+    instr_organized["CH"] = CHstring
+    instr_organized["ROLE"] = ROLEstring
+
+    return instr_organized
+
 
 if __name__ == "__main__":
 
 
-    print(PhysicalChannel.deviceTypes)
-    testQPU = create_QPU_by_route("testQPU","Q1,Q2/RO1/I+Q:DAC=SDAWG_6-1+SDAWG_6-2,SG=DDSLO_4,ADC=SDDIG_2;Q1/XY1/I+Q:DAC=SDAWG_4-1+SDAWG_4-2,SG=DDSLO_3;Q1/Z1:DAC=SDAWG_4-3;")
+    testQPU = create_QPU_by_route("testQPU","Q1,Q2/RO1/I1+Q1:DAC=SDAWG_6-1+SDAWG_6-2,SG=DDSLO_4,ADC=SDDIG_2;Q1/XY1/X1+Y1:DAC=SDAWG_4-1+SDAWG_4-2,SG=DDSLO_3;Q1/Z1:DAC=SDAWG_4-3;")
 
     # availableCh = testQPU.get_IDList_PhysicalChannel()
     # print(f"Avalable channel in QPU {testQPU.id}: {availableCh}")
@@ -227,10 +298,11 @@ if __name__ == "__main__":
     # print(f"Qubit {availableQ[0]}")
     for qid in testQPU.get_IDList_PhysicalQubit():
         print(f"Qubit ID: {qid}")
-
-        for pchid in list(testQPU.QubitSet[qid].PhysicalChannel):
-            pch = testQPU.QubitSet[qid].PhysicalChannel[pchid]
-            print(f"channel ID: {pch.id} Role: {pch.role} coupled: {pch.coupled} devices: {pch.device}")
+        for pchid in list(testQPU.QubitSet[qid].phyCh):
+            pch = testQPU.QubitSet[qid].phyCh[pchid]
+            print(f"channel ID: {pch.id} coupled: {pch.coupled} devices: {pch.device}")
+    
+    print(get_QPUwiring(testQPU))
 
 
 
--- TEST/BETAsite/state_distinguishability/state_distinguishability/iq_kmean.py
+++ TEST/BETAsite/state_distinguishability/state_distinguishability/iq_kmean.py
@@ -1,13 +1,115 @@
 
-from numpy import array, reshape
+from cProfile import label
+from numpy import array, reshape, mean, transpose
 from sklearn.cluster import KMeans
 
 
 
 def get_KmeansSklearn( n_clusters, iqComplex ):
-    data = array([iqComplex.real, iqComplex.imag]).reshape((iqComplex.shape[-1],2))
-    kmeans = KMeans(n_clusters=n_clusters).fit(data)
+    data = complex_to_vector(iqComplex, vectorShape="H")
+    kmeans = KMeans(n_clusters=n_clusters,tol=1e-4).fit(data)
     return kmeans
 
-def get_projectedData_byTwoPt( pts, iqComplex ):
+def complex_to_vector( complexArray, vectorShape="V" ):
+    transArr = array([complexArray.real,complexArray.imag])
+    if vectorShape == "V":
+        return transArr
+    elif "H":
+        return transArr.transpose()
 
+def get_projectedIQVector_byTwoPt( projComplex, iqComplex ):
+    refPoint = mean(projComplex)
+    shiftedIQComplex = iqComplex-refPoint
+    relativeProjComplex = projComplex[0]-refPoint
+    projectionVector = complex_to_vector(array([relativeProjComplex]),"H")
+    shiftedIQVector = complex_to_vector(shiftedIQComplex,"V")
+    projectionMatrix = projectionVector.transpose()@projectionVector/abs(relativeProjComplex)**2
+    projectedVector = projectionMatrix@shiftedIQVector
+    return projectedVector
+
+def get_projectedIQDistance_byTwoPt( projComplex, iqComplex ):
+    refPoint = mean(projComplex)
+    shiftedIQComplex = iqComplex-refPoint
+    relativeProjComplex = projComplex[0]-refPoint
+    projectionVector = complex_to_vector(array([relativeProjComplex]),"H")
+    shiftedIQVector = complex_to_vector(shiftedIQComplex,"V")
+    projectedDistance = projectionVector@shiftedIQVector/abs(relativeProjComplex)
+    return projectedDistance[0]
+
+def get_simulationData(measurementPts, excitedProbability, iqPosition, sigma):
+    excPts = int(measurementPts*excitedProbability)
+    groundProbability = 1-excitedProbability
+    groundPts = int(measurementPts*groundProbability)
+    gpos=iqPosition[0]
+    epos=iqPosition[1]
+    g = np.random.normal(gpos.real, sigma, groundPts)+1j*np.random.normal(gpos.imag, sigma, groundPts)
+    e = np.random.normal(epos.real, sigma, excPts)+1j*np.random.normal(epos.imag, sigma, excPts)
+
+    iqdata = np.append(g,e)    
+    return iqdata
+
+def get_oneShot_kmeanDistance(iqdata):
+
+    km = get_KmeansSklearn(2,iqdata)
+    clusterCenter = km.cluster_centers_.transpose()
+    clusterCenter = clusterCenter[0]+1j*clusterCenter[1]
+    projectedDistance = get_projectedIQDistance_byTwoPt(clusterCenter,iqdata)
+    #b = get_projectedIQVector_byTwoPt(clusterCenter,iqdata)
+    return projectedDistance
+
+def get_oneshot_plot(iqdata,simIQCenter=None):
+    km = get_KmeansSklearn(2,iqdata)
+    clusterCenter = km.cluster_centers_.transpose()
+    clusterCenter = clusterCenter[0]+1j*clusterCenter[1]
+    a = get_projectedIQDistance_byTwoPt(clusterCenter,iqdata)
+    plt.figure(1)
+    plt.plot( iqdata.real, iqdata.imag, "o", label="Data" )
+    plt.plot( clusterCenter.real, clusterCenter.imag, "o", label="KMeans" )
+    #if simIQCenter != None:
+    simCenter = array(simIQCenter).transpose()
+    plt.plot( simCenter.real, simCenter.imag,"ro", label="Simulation" )
+    plt.figure(2)
+    count, bins, ignored = plt.hist(a, 60, density=True)
+    plt.show()
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+
+    simCenter = array([0,1])
+    measurementPts = 10000
+    sigma = 0.2
+    #get_oneshot_plot(get_simulationData(measurementPts,0.5,simCenter,0.25),simIQCenter=simCenter)
+    statisticTest = int(20)
+    ProbabilityRange = np.linspace(0.1,0.9,9)
+    errorDistanceMean = np.empty(ProbabilityRange.shape[-1])
+    errorDistanceSTD = np.empty(ProbabilityRange.shape[-1])
+    for i,excitedProbability in enumerate(ProbabilityRange):
+
+        ed = np.empty(statisticTest)
+        for j in range(statisticTest):
+            km = get_KmeansSklearn(2,get_simulationData(measurementPts,excitedProbability,simCenter,sigma))
+            clusterCenter = km.cluster_centers_.transpose()
+            clusterCenter = clusterCenter[0]+1j*clusterCenter[1]
+            errorDistanceP1 = mean(abs(simCenter.transpose()-clusterCenter))
+
+            clusterCenterP2 = array([clusterCenter[1],clusterCenter[0]])
+            errorDistanceP2 = mean(abs(simCenter.transpose()-clusterCenterP2))
+
+            ed[j] = np.min([errorDistanceP1,errorDistanceP2])
+        errorDistanceMean[i] = np.mean(ed)
+        errorDistanceSTD[i] = np.std(ed)
+        #print(simCenter.transpose(),clusterCenter,clusterCenterP2,errorDistance)
+    #b = get_projectedIQVector_byTwoPt(clusterCenter,iqdata)
+
+    plt.figure(1)
+    plt.errorbar( ProbabilityRange, errorDistanceMean, yerr=errorDistanceSTD, fmt="ro" )
+
+    # plt.plot( clusterCenter.real, clusterCenter.imag, "o", label="KMeans" )
+    # simCenter = array(iqPosition).transpose()
+    # plt.plot( simCenter[0], simCenter[1],"o", label="Simulation" )
+    # #plt.plot( b[0]+mean(clusterCenter).real, b[1]+mean(clusterCenter).imag, "o" )
+    # plt.figure(2)
+    # count, bins, ignored = plt.hist(a, 60, density=True)
+    plt.show()
+    
