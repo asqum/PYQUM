63968da952a360c14a405ab85d84f4b523169374
RatisWu <410614316@gms.ndhu.edu.tw>
2022-08-18 19:20:42 +0800

    CavitySearchWithGaussian Complete
    1) Replace the old CavitySearch.py
    2) Benchmark.py CS-part revise

--- TEST/FACE/pyqum/benchmark.py
+++ TEST/FACE/pyqum/benchmark.py
@@ -992,10 +992,11 @@ from json import dumps
 #---------------load package of load_data---------------
 from pyqum.directive.code.LoadData_lab import jobid_search_pyqum, pyqum_load_data
 #---------------load package of cavity search---------------
-from pyqum.directive.code.CavitySearch import make_amp,make_pha,input_process,output_process,true_alt_info,find_best_ans,db_datamaker,Find_eps,dbscan,predict_dataset,compa_gru_db
-from numpy import array,vstack, hstack
+from pyqum.directive.code.CavitySearch import normalize_1d, peak_info, corr_peak_loc, compu_peak_center_dist, poopoo_filter, rm_empty, gaus, gaussian_fitor, gaussian_filter, amp_pha_compa, ena_clutch_filter
+from scipy.optimize import curve_fit
+from numpy import array, log10, diff, unwrap, arctan2, vstack, hstack, average, max, min, median, std,
+from scipy.signal import savgol_filter as SGF
 from pandas import Series, DataFrame, concat
-from keras.models import load_model
 from pyqum.directive.code.QubitFrequency import colect_cluster,cal_nopecenter,cal_distance,denoise,check_overpower,find_farest,cal_Ec_GHz,freq2idx
 #---------------load package of power dependent---------------
 from sklearn.cluster import KMeans
@@ -1025,72 +1026,117 @@ class Load_From_pyqum:
         return self.amp_data
             
 class CavitySearch:
-    def __init__(self, dataframe):
-        self.dataframe = dataframe
-
-        # ans of prediction
-        self.answer = {}
-        self.amplitude, self.phase, self.freq = [], [], []
-        self.fig = DataFrame()
-        self.ans_array = {}
-
-    def do_analysis(self,designed):
-        I = self.dataframe['I']
-        Q = self.dataframe['Q']
-        self.freq = self.dataframe['Frequency']
-
-        self.amplitude = make_amp(I,Q)
-        self.phase = make_pha(I,Q)
-        self.fig = DataFrame(concat([Series(self.freq),Series(self.amplitude),Series(self.phase)],axis=1))
-
-        # GRU part
-        AMP = load_model(r'C:\Users\ASQUM\Documents\GitHub\PYQUM\TEST\FACE\pyqum\directive\model\GRU_AMP_Accuracy.h5')
-        PHA = load_model(r'C:\Users\ASQUM\Documents\GitHub\PYQUM\TEST\FACE\pyqum\directive\model\GRU_PHA_Accuracy.h5')
-
-        amp, pha, comparison = input_process(self.fig)      # frequency,amplitude,phase; comparison[no.][0] for freq start, end for comparison[no.][1] 
-        self.fig.columns = ['<b>frequency(GHz)</b>','Amplitude','UPhase']
-
-        # prediction GRU
-        amp_pred = AMP.predict(amp)
-        pha_pred = PHA.predict(pha)
-
-        # result process
-        true_out ,alt = output_process(amp_pred,pha_pred,comparison)  
-        zone, voted_amp, voted_pha = true_alt_info(true_out,alt,self.fig)
-
-        gru_ans_amp, status_amp = find_best_ans(zone,voted_amp,self.fig,designed)  # status is the origin predict result with the default peak_limit = 8
-        gru_ans_pha, status_pha = find_best_ans(zone,voted_pha,self.fig,designed)
+    def __init__(self,dataframe):
+        self.df = dataframe
+        self.info = {}
+        self.peak_amp, self.peak_pha = [], []
+        self.sliced_freq = []
+        self.final_answer = []
+        # data pre-smooth : Savitzky-Golay filter
+        self.SGF_width = 11
+        self.polyorder = 1
         
-        # DBSCAN part
-        # dbscan for phase
-        inp_db = db_datamaker(self.phase,self.freq)
-        eps,mini = Find_eps(inp_db) 
-        l_d_pha = dbscan(inp_db,eps,mini)
-        ture_out_db_pha = predict_dataset(l_d_pha,self.freq)
+    
+    def make_amp_uph_from_IQ(self):
+        df = self.df[self.df['Frequency'].between(4,8)]
+        freq = array(df['Frequency'])
+        I = array(df['I'])
+        Q = array(df['Q'])
+        amplitude = normalize_1d(20*log10((I**2+Q**2)**(1/2)))
+        phase = diff(unwrap(arctan2(Q,I)))
+        uphase = normalize_1d(hstack((phase,average(phase))))
+        
+        amplitude -= SGF(amplitude,self.SGF_width,self.polyorder)
+        uphase -= SGF(uphase,self.SGF_width,self.polyorder)
+        
+        p2p = (freq[-1]-freq[0])/freq.shape[0]
 
-        # dbscan for amplitude
-        inp_db = db_datamaker(self.amplitude,self.freq)
-        eps,mini = Find_eps(inp_db) 
-        l_d_amp = dbscan(inp_db,eps,mini)
-        ture_out_db_amp = predict_dataset(l_d_amp,self.freq)
+        compa_fig = concat([Series(freq),Series(amplitude),Series(uphase)],axis=1) 
+        compa_fig.columns = ['Frequency','Amplitude','UPhase']
 
-        true_out_db = vstack((ture_out_db_amp,ture_out_db_pha))
+        self.info = {"Frequency":freq,"Amplitude":amplitude,"UPhase":uphase,"p2p_freq":p2p,"Comparison_fig":compa_fig}
+        
+    def strong_slice(self):
+        freq_shifted = [] 
+        freq_sliced = []    #[[freq_st,freq_ed],[...],...]
+        range_point = int(0.03/self.info['p2p_freq'])
+        for i in range(0,self.info['Frequency'].shape[0]-range_point,range_point):
+            freq_sliced.append([array(self.info['Frequency'][i:i+range_point])[0],array(self.info['Frequency'][i:i+range_point])[-1]])
 
-        zone, voted_amp, voted_pha = true_alt_info(true_out_db,alt,self.fig)
-        db_ans_amp, status_amp = find_best_ans(zone, voted_amp, self.fig, designed)
-        db_ans_pha, status_pha = find_best_ans(zone, voted_pha, self.fig, designed)
+            sliced_amp = array(self.info['Amplitude'][i:i+range_point])
+            sliced_pha = array(self.info['UPhase'][i:i+range_point])
+            min_max_mid_amp = 0.5*(max(sliced_amp)+min(sliced_amp))
+            median_amp = median(sliced_amp)
+            sd_amp = std(sliced_amp)
+            min_max_mid_pha = 0.5*(max(sliced_pha)+min(sliced_pha))
+            median_pha = median(sliced_pha)
+            sd_pha = std(sliced_pha)
+            condi_amp = (median_amp-sd_amp > min_max_mid_amp or min_max_mid_amp > median_amp+sd_amp)
+            condi_pha = (median_pha-sd_pha > min_max_mid_pha or min_max_mid_pha > median_pha+sd_pha)
 
-        amp_ans = [gru_ans_amp,db_ans_amp]
-        pha_ans = [gru_ans_pha,db_ans_pha]
+            if  condi_amp or condi_pha :
+                if i != 0 or i != (self.info['Frequency'].shape[0]-range_point):
+                    if abs(sliced_amp[0]-median_amp) > abs(0.34*(max(sliced_amp)-min(sliced_amp))) or  abs(sliced_pha[0]-median_pha) > abs(0.34*(max(sliced_pha)+min(sliced_pha))):
+                        freq_shifted.append([array(self.info['Frequency'][i-int(range_point/2):i-int(range_point/2)+range_point])[0],array(self.info['Frequency'][i-int(range_point/2):i-int(range_point/2)+range_point])[-1]])
 
-        self.answer = compa_gru_db(amp_ans,pha_ans)   # answer looks: {'0':[start,end],'1':[...],...}
-        return self.answer
+                    if abs(sliced_amp[-1]-median_amp) > abs(0.34*(max(sliced_amp)-min(sliced_amp))) or  abs(sliced_pha[-1]-median_pha) > abs(0.34*(max(sliced_pha)+min(sliced_pha))):
+                        freq_shifted.append([array(self.info['Frequency'][i+int(range_point/2):i+int(range_point/2)+range_point])[0],array(self.info['Frequency'][i+int(range_point/2):i+int(range_point/2)+range_point])[-1]])
+        
+        freq_sliced.extend(freq_shifted)
+        self.sliced_freq = array(freq_sliced) 
+        
+    def give_region(self):
+        region = {}
+    
+        for tip_freq in self.final_answer: 
+            freq = self.info['Comparison_fig'][self.info['Comparison_fig']['Frequency'].between(tip_freq-0.015,tip_freq+0.015)]['Frequency']
+            amp = self.info['Comparison_fig'][self.info['Comparison_fig']['Frequency'].between(tip_freq-0.015,tip_freq+0.015)]['Amplitude']
+            pha = self.info['Comparison_fig'][self.info['Comparison_fig']['Frequency'].between(tip_freq-0.015,tip_freq+0.015)]['UPhase']
+            
+            amp_tip_idx,FWHM_amp = peak_info(amp,self.info['p2p_freq'])  # unit: GHz
+            pha_tip_idx,FWHM_pha = peak_info(pha,self.info['p2p_freq'])
+            avg_tip_idx = 0.5*(array(freq)[amp_tip_idx]+array(freq)[pha_tip_idx])
+            avg_FWHM = 0.5*(FWHM_amp*self.info['p2p_freq']+FWHM_pha*self.info['p2p_freq'])
+            region['%d MHz'%(avg_tip_idx*1000)] = [tip_freq-4*avg_FWHM,tip_freq+4*avg_FWHM]
 
-    def give_answer_array(self,no):
+        self.final_answer = region  #{'5487 MHz':[freq_start,freq_end],'... MHz':[...],....}
+        
+    def amp_pha_compa(self):
+        amp_loc_array = list(self.peak_amp)
+        pha_loc_array = list(self.peak_pha)
+        nearest = []
+        for amp_peak in amp_loc_array:
+            for pha_peak in pha_loc_array: 
+                if abs(amp_peak - pha_peak)<0.01:
+                    near_center_freq = compu_peak_center_dist(amp_peak,pha_peak,self.info['Comparison_fig'],self.info['p2p_freq'])
+                    nearest.append(near_center_freq)
+                
+        self.final_answer = ena_clutch_filter(list(set(poopoo_filter(array(nearest),self.info['Comparison_fig']))))    # 1D array contain tip freq [freq1,freq2,...]
+          
+    # to call below do analysis                     
+    def do_analysis(self): 
+        self.make_amp_uph_from_IQ()
+        self.strong_slice()
+        gaussian_exist = {"peak_freq_amp":[],"peak_freq_pha":[]}
+        for i in range(self.sliced_freq.shape[0]):
+            peak_amp, peak_pha =  gaussian_filter(self.sliced_freq[i],self.info['Comparison_fig'],self.info['p2p_freq'])
+
+            gaussian_exist['peak_freq_amp'].append(peak_amp)
+            gaussian_exist['peak_freq_pha'].append(peak_pha)
+
+        self.peak_amp = rm_empty(gaussian_exist["peak_freq_amp"])
+        self.peak_pha = rm_empty(gaussian_exist["peak_freq_pha"])
+        self.amp_pha_compa()
+        self.give_region()
+
+        return self.final_answer  # return out {'5487 MHz':[freq_start,freq_end],'... MHz':[...],....}
+        
+    #to call below send arrays to js plotly            
+    def give_js_info(self,freq_MHz):
         self.ans_array = {
-        'Frequency':self.fig[self.fig["<b>frequency(GHz)</b>"].between(self.answer[str(no)][0],self.answer[str(no)][1])]['<b>frequency(GHz)</b>'],
-        'Amplitude':self.fig[self.fig["<b>frequency(GHz)</b>"].between(self.answer[str(no)][0],self.answer[str(no)][1])]['Amplitude'],
-        'UPhase':self.fig[self.fig["<b>frequency(GHz)</b>"].between(self.answer[str(no)][0],self.answer[str(no)][1])]['UPhase']
+        'Frequency':array(self.info['Comparison_fig'][self.info['Comparison_fig']['Frequency'].between(self.answer['%d MHz'%freq_MHz][0],self.answer['%d MHz'%freq_MHz][1])]['Frequency']),
+        'Amplitude':array(self.info['Comparison_fig'][self.info['Comparison_fig']['Frequency'].between(self.answer['%d MHz'%freq_MHz][0],self.answer['%d MHz'%freq_MHz][1])]['Amplitude']),
+        'UPhase':array(self.info['Comparison_fig'][self.info['Comparison_fig']['Frequency'].between(self.answer['%d MHz'%freq_MHz][0],self.answer['%d MHz'%freq_MHz][1])]['UPhase'])
         }
 
 class PowerDepend:
@@ -1321,8 +1367,8 @@ class AutoScan1Q:
         print("do measurement\n")
         self.jobid_dict["CavitySearch"] = jobid
         dataframe = Load_From_pyqum(jobid).load()
-        # self.cavity_list = CavitySearch(dataframe).do_analysis(self.numCPW) #model h5 cannot import
-        self.cavity_list = {'7116.0 MHz': [7.102, 7.128], '6334.0 MHz': [6.32, 6.346]}
+        self.cavity_list = CavitySearch(dataframe).do_analysis() #model h5 cannot import <- 0818 update, no need it anymore
+        #self.cavity_list = {'7116.0 MHz': [7.102, 7.128], '6334.0 MHz': [6.32, 6.346]}
         self.total_cavity_list = list(self.cavity_list.keys())
         self.readout_para = {i: {} for i in self.total_cavity_list}
         self.readout_para["cavity_list"] = self.cavity_list
